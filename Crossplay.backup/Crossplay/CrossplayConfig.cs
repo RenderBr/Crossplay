ba691e6f5d99139dbb17ff3fc443b96bf251774a
Julian <julianmuchacho666@gmail.com>
2022-10-24 01:01:18 -0400

    updated to otapi3

---
+++ Crossplay.backup/Crossplay/CrossplayConfig.cs
@@ -0,0 +1,21 @@
+﻿using System.IO;
+using Newtonsoft.Json;
+using TShockAPI.Configuration;
+
+namespace Crossplay
+{
+    public class CrossplaySettings
+    {
+        public bool EnableJourneySupport = false;
+
+        public bool EnablePacketDebugging = false;
+
+        public bool FakeVersionEnabled = false;
+
+        public int FakeVersion = 248;
+    }
+
+    public class CrossplayConfig : ConfigFile<CrossplaySettings>
+    {
+    }
+}
